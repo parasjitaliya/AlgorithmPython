9c66b2ce7446f26160e9acbf9deafa75d6abd2ad
paras <parasjitaliya@gmail.com>
2020-02-17 12:00:28 +0530

    Added:anagram logic for prime number in any range

--- utilitylogic.py
+++ utilitylogic.py
@@ -12,3 +12,18 @@ class numberlogic:
             print("Strings are anagram")
         else:
             print("Strings are not anagram")
+
+    # ------------------prime number range-----------------------------
+    def prime_range(self, begin_range, end_range):
+        prime_numbers_list = []
+        for number in range(begin_range, end_range + 1):
+            if number == 0 or number == 1:
+                continue
+            flag = 0
+            for i in range(2, (number // 2) + 1):
+                if number % i == 0:
+                    flag = 1
+                    break
+            if (flag == 0):
+                prime_numbers_list.append(number)
+        return prime_numbers_list
