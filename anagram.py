1d86f6bbbcd9eb19bc548a930bca64d00ce83f3d
paras <parasjitaliya@gmail.com>
2020-02-17 11:56:38 +0530

    Added:anagram flile for check string is anagram or not

---
+++ anagram.py
@@ -0,0 +1,7 @@
+""" take first string and check with second input string for the two string are anagram or not"""
+from utilitylogic import numberlogic
+obj = numberlogic()
+str1= input("enter the first string for check anagram:")
+str2 = input("enter the second string for check anagram:")
+
+obj.anagram_string(str1, str2)
