8fd20fe886b9414136eafff32d3f63abf2e1c6cd
paras <parasjitaliya@gmail.com>
2020-02-17 14:59:20 +0530

    Added:logical formula for day of week

--- utilitylogic.py
+++ utilitylogic.py
@@ -160,3 +160,11 @@ class generallogic:
             i += 1
         return total_notes
 
+    # -----------------------------------day of week logic --------------------------------------
+    def day_of_week(self, d, m, y, days):
+        y0 = y - (14 - m) // 12
+        x = y0 + (y0 // 4) - y0 // 100 + y0 // 400
+        m0 = m + 12 * ((14 - m) // 12) - 2
+        d0 = (d + x + (31 * m0) // 12) % 7
+        print(f"Day of the week is {days[d0]}")
+
